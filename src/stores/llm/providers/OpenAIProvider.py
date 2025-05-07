208a1795ba8bc62c553642fbc74e941addfec6ad
Al forsan <rewansaraya@gmail.com>
2025-05-07 20:24:06 +0300

    update

--- src/stores/llm/providers/OpenAIProvider.py
+++ src/stores/llm/providers/OpenAIProvider.py
@@ -7,14 +7,14 @@ class OpenAIProvider(LLMInterface):
 
     def __init__(self, api_key: str, api_url:str = None,
                        default_input_max_characters: int=1000,
-                       default_generation_max_output_characters: int=1000,
+                       default_generation_max_output_tokens: int=1000,
                        default_generation_temperature: float=0.1 ):
 
         self.api_key = api_key
         self.api_url = api_url
 
         self.default_input_max_characters = default_input_max_characters
-        self.default_generation_max_output_characters = default_generation_max_output_characters
+        self.default_generation_max_output_tokens = default_generation_max_output_tokens
         self.default_generation_temperature = default_generation_temperature
 
         self.generation_model_id = None
@@ -28,3 +28,45 @@ class OpenAIProvider(LLMInterface):
         )
 
         self.logger = logging.getLogger(__name__)
+
+    def set_generation_model(self, model_id: str):
+        self.generation_model_id = model_id
+
+    def set_embedding_model(self, model_id: str, embedding_size: int):
+        self.embedding_model_id = model_id
+        self.embedding_size = embedding_size
+
+    def generate_text(self, prompt: str, chat_history: list = [], max_output_tokens: int = None,
+                      temperature: float = None):
+
+        if not self.client:
+            self.logger.error("OpenAI client wes not set")
+            return  None
+
+        if not self.generation_model_id:
+            self.logger.error("Generation model for OpenAI was not set")
+            return None
+
+        max_output_tokens = max_output_tokens if max_output_tokens else self.default_generation_max_output_tokens
+        temperature = temperature if temperature else self.default_generation_temperature
+
+    def embed_text(self, text: str, document_type: str = None):
+
+        if not self.client:
+            self.logger.error("OpenAI client was not set")
+            return None
+
+        if not self.embedding_model_id:
+            self.logger.error("Embedding model for OpenAI was not set")
+            return None
+
+        response = self.client.embeddings.create(
+            model= self.embedding_model_id,
+            input= text,
+        )
+
+        if not response or not response.data or len(response.data) == 0 or not response.data[0].embedding:
+            self.logger.error("Error while embedding text with OpenAI")
+            return None
+
+        return response.data[0].embedding
