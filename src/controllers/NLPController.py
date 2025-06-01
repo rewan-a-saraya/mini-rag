81fc6962ba047330bf95bd709dcc4c55ea785de0
Al forsan <rewansaraya@gmail.com>
2025-06-01 16:57:46 +0300

    update

--- src/controllers/NLPController.py
+++ src/controllers/NLPController.py
@@ -1,5 +1,9 @@
+from sqlalchemy.testing.suite.test_reflection import metadata
+
 from .BaseController import BaseController
-from src.models.db_schemes import Project
+from src.models.db_schemes import Project, DataChunk
+from src.stores.llm.LLMEnums import DocumentTypeEnum
+from typing import List
 
 class NLPController(BaseController):
 
@@ -14,3 +18,45 @@ class NLPController(BaseController):
         return f"collection_{project_id}".strip()
 
     def reset_vector_db_collection(self,project: Project):
+        collection_name = self.create_collection_name(project_id=project.project_id)
+
+        return self.vectordb_client.delete_collection(collection_name=collection_name)
+
+    def get_vector_db_collection_info(self, project: Project):
+        collection_name = self.create_collection_name(project_id=project.project_id)
+        collection_info = self.vectordb_client.get_collection_info(collection_name=collection_name)
+
+        return collection_info
+
+    def index_into_vector_db(self, project:Project, chunks: List[DataChunk],
+                             do_reset: bool = False):
+
+        # step 1 : get collection name
+        collection_name = self.create_collection_name(project_id=project.project_id)
+
+        # step 2 : manage items
+        texts = [c.chunk_text for c in chunks]
+        metadata = [c.chunk_metadata for c in chunks]
+
+        vector = [
+            self.embedding_client.embed_text(text=texts,
+                                             document_type=DocumentTypeEnum.DOCUMENT.value)
+            for text in texts
+        ]
+
+        # step 3 : create collection if not exists
+        _ = self.vectordb_client.create_collection(
+            collection_name=collection_name,
+            embedding_size=self.embedding_client.embedding_size,
+            do_reset=do_reset,
+        )
+
+        # step 4 : insert into vector db
+        _ = self.vectordb_client.insert_many(
+            collection_name=collection_name,
+            texts=texts,
+            metadata=metadata,
+            vectors=vector,
+        )
+
+        return True
